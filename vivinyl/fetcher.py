d956fe1fe1c52a308388c18b4fd66dbcdab77985
janekkrukowski <janekkrukowski@gmail.com>
2017-02-28 00:33:32 +0100

    added running tasks in parallel

--- vivinyl/fetcher.py
+++ vivinyl/fetcher.py
@@ -20,8 +20,7 @@ logging.config.fileConfig('./vivinyl/logging.conf')
 logger = logging.getLogger('vivinyl')
 
 class DataFetcher(object):
-    def __init__(self, ids, data_parser, downloader, datalogger):
-        self.ids = ids
+    def __init__(self, data_parser, downloader, datalogger):
         self.data_parser = data_parser
         self.downloader = downloader
         self.datalogger = datalogger
@@ -34,45 +33,45 @@ class DataFetcher(object):
         remaining = response.headers.get('X-Discogs-Ratelimit-Remaining', self.remaining)
         return limit, used, int(remaining)
 
-    async def fetch(self, session, url, num):
-        async with session.get(url, headers=headers, params=params) as response:
-            limit, used, self.remaining = self.get_header_values(response)
-            logger.info('{0}: Limit {1} Used {2} Remaining {3}'.format(num, limit, used, self.remaining))
-            if response.status != 200:
-                logger.info('{0}: Response status {1} for release'.format(num, response.status))
-                return None
-            return await response.json()
-
-    async def process(self):
+    async def fetch(self, url, num):
         async with aiohttp.ClientSession() as session:
-            for num in self.ids:
-                logger.info('{0}: Started...'.format(num))
-                url = self.build_url(num)
-                if self.remaining <= self.min_remaining:
-                    logger.info('{0}: Remaining {1} sleeping'.format(num, self.remaining))
-                    await asyncio.sleep(1)
-                data = await self.fetch(session=session, url=url, num=num)
-                if not data:
-                    continue
-                self.datalogger.save(data=data.get('images'), num=num)
-                image_url = self.data_parser.get_image_uri(data=data, num=num)
-                if not image_url:
-                    logger.info('{0}: No image url'.format(num))
-                    continue
-                await self.downloader.download(url=image_url, num=num)
+            async with session.get(url, headers=headers, params=params) as response:
+                limit, used, self.remaining = self.get_header_values(response)
+                logger.info('{0}: Limit {1} Used {2} Remaining {3}'.format(num, limit, used, self.remaining))
+                if response.status != 200:
+                    logger.info('{0}: Response status {1} for release'.format(num, response.status))
+                    return None
+                return await response.json()
+
+    async def process(self, num):
+        logger.info('{0}: Started...'.format(num))
+        url = self.build_url(num)
+        if self.remaining <= self.min_remaining:
+            logger.info('{0}: Remaining {1} sleeping'.format(num, self.remaining))
+            await asyncio.sleep(1)
+        data = await self.fetch(url=url, num=num)
+        if not data:
+            return
+        self.datalogger.save(data=data.get('images'), num=num)
+        image_url = self.data_parser.get_image_uri(data=data, num=num)
+        if not image_url:
+            logger.info('{0}: No image url'.format(num))
+            return
+        await self.downloader.download(url=image_url, num=num)
 
     def build_url(self, num):
         return 'https://api.discogs.com/releases/{0}'.format(num)
 
 
-async def main(ids):
+async def main(start, stop, workers, loop):
     downloader = ImageDownloader(folder_path='./data')
     datalogger = DataLogger(folder_path='./logs')
-    fetcher = DataFetcher(ids=ids,
-                          data_parser=DataParser(),
+    fetcher = DataFetcher(data_parser=DataParser(),
                           downloader=downloader,
                           datalogger=datalogger)
-    await fetcher.process()
+    ids = range(start, stop, workers)
+    for release_id in ids:
+        await asyncio.wait([fetcher.process(release_id+i) for i in range(workers)], loop=loop)
 
 
 if __name__ == '__main__':
@@ -82,4 +81,5 @@ if __name__ == '__main__':
     parser.add_argument('stop', type=int)
     args = parser.parse_args()
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(range(args.start, args.stop)))
+    loop.run_until_complete(main(start=args.start, stop=args.stop, workers=3, loop=loop))
+    loop.close()
