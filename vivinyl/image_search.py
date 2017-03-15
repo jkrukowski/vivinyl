2520b6ed615624db07a42a23fd4dec519f4edb35
janekkrukowski <janekkrukowski@gmail.com>
2017-03-15 20:53:36 +0100

    function to match file

--- vivinyl/image_search.py
+++ vivinyl/image_search.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from elasticsearch import Elasticsearch
 from image_match.elasticsearch_driver import SignatureES
@@ -9,12 +10,31 @@ def get_files(folder='./data'):
             yield os.path.join(folder, file)
 
 
-def main():
+def add_files():
     es = Elasticsearch()
     ses = SignatureES(es)
     for path in get_files():
         ses.add_image(path)
 
 
+def match_file(file_path):
+    es = Elasticsearch()
+    ses = SignatureES(es)
+    result = ses.search_image(file_path, all_orientations=True)
+    print(result)
+    return result
+
+
+def main(args):
+    if args.a:
+        add_files()
+    else:
+        match_file(args.match)
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('a', action='store_false')
+    parser.add_argument('--match', type=str, default='img_1.jpg')
+    args = parser.parse_args()
+    main(args)
